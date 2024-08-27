cd4dcac4ed58f17385ecd729a1dcd781b811f769
oleksandr.piskurov <oleksandr.piskurov@mac-MT41YG9TJ6.local>
2024-08-28 00:26:43 +0300

    Update

--- app/utils/request.ts
+++ app/utils/request.ts
@@ -1,10 +1,11 @@
-function sendRequest(method: string, url: string, data={}): string {
+export function sendRequest(method: string, url: string, data={}) {
    const xhr = new XMLHttpRequest();
    xhr.open(method, url, false);
    xhr.setRequestHeader('Content-Type', 'application/json');
+   xhr.setRequestHeader('Authorization', `Bearer {process.env.TOKEN}`);
    xhr.send(JSON.stringify(data));
    if (xhr.status === 200) {
-      return xhr.responseText;
+      return xhr.response;
    } else {
       throw new Error('Request failed: ' + xhr.statusText);
    }
--- handlers/ping_me.ts
+++ handlers/ping_me.ts
@@ -1,2 +1,4 @@
+import {sendRequest} from "../app/utils/request";
+
 console.log('token!');
-console.log(process.env.TOKEN);
+console.log(sendRequest('GET', 'web:8000/api/condition/'));

7c189fe911f555fbb07288194192112b1ca706bc
oleksandr.piskurov <oleksandr.piskurov@mac-MT41YG9TJ6.local>
2024-08-28 21:14:31 +0300

    Update

--- utils/http.py
+++ utils/http.py
@@ -2,10 +2,12 @@ import os
 
 import requests
 
+from constants import BASE_URL
+
 
 def send_request(method, url, data=None, headers=None):
     data = data or {}
     headers = headers or {}
 
     headers['Authorization'] = f'Bearer {os.environ["TOKEN"]}'
-    return getattr(requests, method)(url, data=data, headers=headers)
+    return getattr(requests, method)(BASE_URL +  url, data=data, headers=headers)

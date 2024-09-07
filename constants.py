eee94f2a4f0b77fc0f6b71c072f1fa2171562aab
oleksandr.piskurov <oleksandr.piskurov@mac-MT41YG9TJ6.local>
2024-09-07 16:17:05 +0300

    Update

--- constants.py
+++ constants.py
@@ -5,6 +5,6 @@ print(os.environ.get('BASE_URL'))
 print(os.environ.get('env.BASE_URL'))
 print(os.environ)
 
-BASE_URL = os.environ.get('BASE_URL')
+BASE_URL = os.environ.get('base_url')
 
 DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%s%z'

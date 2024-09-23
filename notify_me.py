58187041b071b73186004ae3ad03051018d7f71d
oleksandr.piskurov <oleksandr.piskurov@mac-MT41YG9TJ6.local>
2024-09-23 18:07:48 +0300

    Udate

--- notify_me.py
+++ notify_me.py
@@ -1,18 +1,9 @@
-from datetime import datetime, timedelta, time
-
 import sys
-from typing import List
 
-from app.calendar.models import RegularEvent, Event
 from app.notification.models import Message, NotificationTransport
-from app.profile.models import Profile
 
 sys.path.append('./')
 
 desktop = NotificationTransport.desktop()
-print(desktop.id)
 
-
-r = Message.simple_message(transport=desktop)
-print('******')
-print(r)
+r = Message.simple_message(transport=desktop, extra_data={'title': 'You have been notified'})

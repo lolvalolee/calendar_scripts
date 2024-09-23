f711d02910aa4dc6571627a4f0a4e58edcdac7f3
oleksandr.piskurov <oleksandr.piskurov@mac-MT41YG9TJ6.local>
2024-09-23 15:08:05 +0300

    Udate

--- utils/models.py
+++ utils/models.py
@@ -70,7 +70,8 @@ class CRUDModel(BaseModel):
         r = None
 
         for k, v in kwargs.items():
-            kwargs[k] = v.id
+            if isinstance(v, BaseModel):
+                kwargs[k] = v.id
 
         try:
             r = send_request('post', cls.combine_url(cls.url), data=kwargs)

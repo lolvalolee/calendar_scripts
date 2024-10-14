d0c4c973de7b04def6ce3292d07d7e0cc7fc4f20
oleksandr.piskurov <oleksandr.piskurov@mac-MT41YG9TJ6.local>
2024-10-15 02:42:35 +0300

    Update

--- notify_me.py
+++ notify_me.py
@@ -1,9 +1,31 @@
+import os
 import sys
 
+from openai import OpenAI
+
 from app.notification.models import Message, NotificationTransport
+
 
 sys.path.append('./')
 
 desktop = NotificationTransport.desktop()
 
-r = Message.simple_message(transport=desktop, extra_data={'title': 'This is a message'})
+
+api_key = os.environ["api_key"]
+
+client = OpenAI(api_key=api_key, project='proj_aqopDeyhEzIZzEK4PrePdFVt')
+
+completion = client.chat.completions.create(
+    model="gpt-3.5-turbo-0125",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            # "content": "Напиши сообщение о том что сон полезен для здоровья. Сообщение должно хоть немного отличаться от предыдущего.",
+            "content": "Похвали меня."
+
+}
+    ]
+)
+
+r = Message.simple_message(transport=desktop, extra_data={'title': completion.choices[0].message.content})

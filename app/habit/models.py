0bad33fd9996e93f004198221c983b0c6817a07c
oleksandr.piskurov <oleksandr.piskurov@mac-MT41YG9TJ6.local>
2024-11-04 10:26:59 +0200

    Update

---
+++ app/habit/models.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+from datetime import datetime
+from typing import List
+
+from utils.models import apply_default_filters, CRUDModel
+
+
+@dataclass
+class UserHabitRecord(CRUDModel):
+    url = '/api/habit-report/'
+
+    result: str
+    record_date: datetime
+    user_habit: int
--- eothyrox_habit.py
+++ eothyrox_habit.py
@@ -3,9 +3,14 @@ import sys
 
 from openai import OpenAI
 
+from app.habit.models import UserHabitRecord
 from app.notification.models import Message, NotificationTransport
 
 
 sys.path.append('./')
 print('object_id : ')
 print(os.environ["object_id"])
+objects = UserHabitRecord.get_objects()
+obj = UserHabitRecord.get_object(os.environ["object_id"])
+print(objects)
+print(obj)

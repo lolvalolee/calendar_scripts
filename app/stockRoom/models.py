cf03125256abf6a27afe2a49b360fd77b0f5e302
oleksandr.piskurov <oleksandr.piskurov@mac-MT41YG9TJ6.local>
2024-11-04 19:03:34 +0200

    Update

---
+++ app/stockRoom/models.py
@@ -0,0 +1,53 @@
+from dataclasses import dataclass
+from datetime import datetime, date, time
+from typing import List
+
+from utils.models import apply_default_filters, CRUDModel
+
+
+@dataclass
+class Stock(CRUDModel):
+    url = '/api/stock/'
+
+    name: str
+    user_group: dict
+    user_habit: int
+    isolate_stock_items: bool
+    default_encryption_keys: list
+
+
+
+
+
+
+#
+#
+# @dataclass
+# class UserHabit(CRUDModel):
+#     url = '/api/user-habit/'
+#
+#     name: dict
+#     name_encrypt: str
+#     description: str
+#     name_on_skip: str
+#     name_on_fail: str
+#     name_on_complete: str
+#     count_daily: int
+#     on_skip: int
+#     on_fail: int
+#     on_complete: int
+#     default_choice: str
+#     notification_time: time
+#     is_system: bool
+#     records: list
+#     last_completed_record: dict
+#     last_skipped_record: dict
+#     last_failed_record: dict
+#     created: datetime
+#     changed: datetime
+#
+#     count_today: int = 0
+#     count_done: int = 0
+#     count_skipped: int = 0
+#     count_failed: int = 0
+#     count_left: int = 0
--- eothyrox_habit.py
+++ eothyrox_habit.py
@@ -8,9 +8,9 @@ from app.notification.models import Message, NotificationTransport
 
 
 sys.path.append('./')
-print('object_id : ')
-print(os.environ["object_id"])
+
 objects = UserHabitRecord.get_objects()
 user_habit_record = UserHabitRecord.get_object(os.environ["object_id"])
 user_habit = UserHabit.get_object(user_habit_record.user_habit)
-print(user_habit)
+
+stock =

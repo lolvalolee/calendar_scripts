8646bff2c709b07627cbf15bb89d3bff95b2427c
oleksandr.piskurov <oleksandr.piskurov@mac-MT41YG9TJ6.local>
2024-11-04 10:58:36 +0200

    Update

--- app/habit/models.py
+++ app/habit/models.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from datetime import datetime
+from datetime import datetime, date, time
 from typing import List
 
 from utils.models import apply_default_filters, CRUDModel
@@ -12,3 +12,27 @@ class UserHabitRecord(CRUDModel):
     result: str
     record_date: datetime
     user_habit: int
+
+
+@dataclass
+class UserHabit(CRUDModel):
+    url = '/api/user-habit/'
+
+    record_date = date
+    name_encrypt = str
+    description = str
+    name_on_skip = str
+    name_on_fail = str
+    name_on_complete = str
+    count_daily = int
+    on_skip = int
+    on_fail = int
+    on_complete = int
+    default_choice = str
+    notification_time = time
+    is_system = bool
+    count_today = int
+    count_done = int
+    count_skipped = int
+    count_failed = int
+    count_left = int
--- eothyrox_habit.py
+++ eothyrox_habit.py
@@ -3,7 +3,7 @@ import sys
 
 from openai import OpenAI
 
-from app.habit.models import UserHabitRecord
+from app.habit.models import UserHabitRecord, UserHabit
 from app.notification.models import Message, NotificationTransport
 
 
@@ -11,6 +11,6 @@ sys.path.append('./')
 print('object_id : ')
 print(os.environ["object_id"])
 objects = UserHabitRecord.get_objects()
-obj = UserHabitRecord.get_object(os.environ["object_id"])
-print(objects)
-print(obj)
+user_habit_record = UserHabitRecord.get_object(os.environ["object_id"])
+user_habit = UserHabit.get_object(user_habit_record.user_habit)
+print(user_habit)

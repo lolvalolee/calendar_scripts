750a6d956ddf780da25d49226b8aa500803971d1
oleksandr.piskurov <oleksandr.piskurov@mac-MT41YG9TJ6.local>
2024-11-04 23:32:08 +0200

    Update

--- app/stockRoom/models.py
+++ app/stockRoom/models.py
@@ -15,7 +15,7 @@ class Stock(CRUDModel):
 
     def use(self):
         data = {'stock': self.id}
-        r = send_request('post', self.combine_url(self.url + '/use/'), data=data)
+        r = send_request('post', self.combine_url(self.url + 'use/'), data=data)
         print('use item')
         print(r)
 

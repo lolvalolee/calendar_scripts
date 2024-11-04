86b7c9dc3177d235f5111da7772e18e3b49ceb6d
oleksandr.piskurov <oleksandr.piskurov@mac-MT41YG9TJ6.local>
2024-11-04 11:10:54 +0200

    Update

--- app/habit/models.py
+++ app/habit/models.py
@@ -18,27 +18,27 @@ class UserHabitRecord(CRUDModel):
 class UserHabit(CRUDModel):
     url = '/api/user-habit/'
 
-    record_date = date
-    name_encrypt = str
-    description = str
-    name_on_skip = str
-    name_on_fail = str
-    name_on_complete = str
-    count_daily = int
-    on_skip = int
-    on_fail = int
-    on_complete = int
-    default_choice = str
-    notification_time = time
-    is_system = bool
-    count_today = int
-    count_done = int
-    count_skipped = int
-    count_failed = int
-    count_left = int
-    records = list
-    last_completed_record = dict
-    last_skipped_record = dict
-    last_failed_record = dict
-    created = datetime
-    changed = datetime
+    record_date: date
+    name_encrypt: str
+    description: str
+    name_on_skip: str
+    name_on_fail: str
+    name_on_complete: str
+    count_daily: int
+    on_skip: int
+    on_fail: int
+    on_complete: int
+    default_choice: str
+    notification_time: time
+    is_system: bool
+    count_today: int
+    count_done: int
+    count_skipped: int
+    count_failed: int
+    count_left: int
+    records: list
+    last_completed_record: dict
+    last_skipped_record: dict
+    last_failed_record: dict
+    created: datetime
+    changed: datetime

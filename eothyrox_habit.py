ad2075308f3bb8328a09088e5f9ddebfe1e62764
oleksandr.piskurov <oleksandr.piskurov@mac-MT41YG9TJ6.local>
2024-11-05 13:53:31 +0200

    Update

--- eothyrox_habit.py
+++ eothyrox_habit.py
@@ -11,15 +11,15 @@ user_habit_record = UserHabitRecord.get_object(os.environ["object_id"])
 user_habit = UserHabit.get_object(user_habit_record.user_habit)
 
 stock_item = StockItem.get_objects(name='Eothyrox')[0]
-measure = Measure.get_objects(name='Штук')[0]
+pieces = Measure.get_objects(name='Штук')[0]
+pack = Measure.get_objects(name='Упаковка')[0]
 stock = Stock.get_objects(name='Private and isolated')[0]
 
-user_stock_room_item = UserStockRoomItem.get_objects(stock_room_item=stock_item.id, measure=measure.id)
-print('!!!!!')
-print(user_stock_room_item)
-
-
-# stock.use()
-#
-# print('stock:')
-# print(stock)
+user_stock_room_item = UserStockRoomItem.get_objects(stock_room_item=stock_item.id, measure=pieces.id)
+if not user_stock_room_item:
+    print('no items')
+    user_stock_room_item = UserStockRoomItem.get_objects(stock_room_item=stock_item.id, measure=pack.id)
+    print('exists!!')
+else:
+    print('!!!!!')
+    print(user_stock_room_item)

4c8700928877a6263907e01d5a877c49e8b48f65
oleksandr.piskurov <oleksandr.piskurov@mac-MT41YG9TJ6.local>
2024-11-05 15:13:42 +0200

    Update

--- eothyrox_habit.py
+++ eothyrox_habit.py
@@ -12,7 +12,7 @@ user_habit = UserHabit.get_object(user_habit_record.user_habit)
 
 stock_item = StockItem.get_objects(name='Eothyrox')[0]
 pieces = Measure.get_objects(name='Штук')[0]
-pack = Measure.get_objects(name='Упаковка')[0]
+pack = Measure.get_objects(name='Упаковок')[0]
 stock = Stock.get_objects(name='Private and isolated')[0]
 
 user_stock_room_item = UserStockRoomItem.get_objects(stock_room_item=stock_item.id, measure=pieces.id)

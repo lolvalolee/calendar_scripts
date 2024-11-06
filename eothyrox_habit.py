42a89a1798ba75d81d1212662bb67d9da9419a31
oleksandr.piskurov <oleksandr.piskurov@mac-MT41YG9TJ6.local>
2024-11-06 08:34:44 +0200

    Update

--- eothyrox_habit.py
+++ eothyrox_habit.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 from app.habit.models import UserHabitRecord, UserHabit
+from app.notification.models import Message, NotificationTransport
 from app.stockRoom.constants import STATUS_IN_STOCK_ROOM, STATUS_PLANNED
 from app.stockRoom.models import Stock, StockItem, Measure, UserStockRoomItem
 
@@ -10,6 +11,9 @@ sys.path.append('./')
 objects = UserHabitRecord.get_objects()
 user_habit_record = UserHabitRecord.get_object(os.environ["object_id"])
 user_habit = UserHabit.get_object(user_habit_record.user_habit)
+
+desktop = NotificationTransport.desktop()
+telegram = NotificationTransport.telegram()
 
 stock_item, _ = StockItem.get_objects(name='Eothyrox')
 piece, _ = Measure.get_objects(name='Штук')
@@ -37,11 +41,9 @@ else:
     stock.use(stock_item.id, piece.id, 1)
 
 if count < 10 and packs_count == 0:
-    print('need plane item!')
-    print(count)
     planned = UserStockRoomItem.get_objects_count(stock_room_item=stock_item.id, measure=pack.id,
                                             status=STATUS_PLANNED)
-    print('planned')
-    print(planned)
     if planned == 0:
         stock.plane(stock_item.id, pack.id, 2)
+        [Message.simple_message(transport=desktop, extra_data={'title': 'Eothyrox добавлен в список покупок'})
+         for transport in [desktop, telegram]]
--- utils/models.py
+++ utils/models.py
@@ -53,8 +53,6 @@ class BaseModel:
     @classmethod
     def get_objects_count(cls, **kwargs):
         data, count = cls.get_objects(**kwargs)
-        print('!!!')
-        print([item.count for item in data])
         return sum(map(lambda item: float(item.count), data))
 
     def __post_init__(self):

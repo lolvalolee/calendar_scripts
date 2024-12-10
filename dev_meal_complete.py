2894988ccab03d6a72265ba47930a01ab7a458e8
oleksandr.piskurov <oleksandr.piskurov@mac-MT41YG9TJ6.local>
2024-12-10 23:18:08 +0200

    UPdate

--- dev_meal_complete.py
+++ dev_meal_complete.py
@@ -5,14 +5,15 @@ from app.notification.models import Message, NotificationTransport
 from app.stockRoom.models import Meal, MealItem
 
 sys.path.append('./')
+#
+# desktop = NotificationTransport.desktop()
+# meal = Meal.get_object(os.environ["object_id"])
+#
+# msg = ''
+#
+# for item in meal.meal_items:
+#     meal_item = MealItem.get_object(item['id'])
+#     msg += f'{meal_item.debt} {meal_item.measure["short_name"]} {meal_item.stock_room_item["name"]["value"]}'
+#
+# Message.simple_message(transport=desktop, extra_data={'title': f'Message :{msg}'})
 
-desktop = NotificationTransport.desktop()
-meal = Meal.get_object(os.environ["object_id"])
-
-msg = ''
-
-for item in meal.meal_items:
-    meal_item = MealItem.get_object(item['id'])
-    msg += f'{meal_item.debt} {meal_item.measure["short_name"]} {meal_item.stock_room_item["name"]["value"]}'
-
-Message.simple_message(transport=desktop, extra_data={'title': f'Message :{msg}'})

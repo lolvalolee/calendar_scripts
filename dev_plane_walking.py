a07d408abd19966090985d85e09670658183fb62
oleksandr.piskurov <oleksandr.piskurov@mac-MT41YG9TJ6.local>
2024-12-31 13:54:05 +0200

    Update

--- dev_plane_walking.py
+++ dev_plane_walking.py
@@ -12,17 +12,6 @@ from app.notification.models import Message, NotificationTransport
 from app.profile.models import Profile
 from app.stockRoom.models import Meal, MealItem, MealSchedule, Measure, Stock, Recipe, StockItem
 
-#
-# desktop = NotificationTransport.desktop()
-# meal = Meal.get_object(os.environ["object_id"])
-#
-# msg = ''
-#
-# for item in meal.meal_items:
-#     meal_item = MealItem.get_object(item['id'])
-#     msg += f'{meal_item.debt} {meal_item.measure["short_name"]} {meal_item.stock_room_item["name"]["value"]}'
-#
-# Message.simple_message(transport=desktop, extra_data={'title': f'Message :{msg}'})
 
 profile = Profile.get()
 tz = ZoneInfo(profile.timezone)

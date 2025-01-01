5b293fb3105479fdded1082b03a33f9c40b00044
oleksandr.piskurov <oleksandr.piskurov@mac-MT41YG9TJ6.local>
2025-01-01 07:44:55 +0200

    UPdate

--- dev_plane_walking.py
+++ dev_plane_walking.py
@@ -1,7 +1,8 @@
 import sys
-from datetime import datetime, timedelta
+from datetime import timedelta
 
 from app.calendar.models import RegularEvent, Event
+from app.notification.models import Message, NotificationTransport
 
 sys.path.append('./')
 
@@ -18,9 +19,7 @@ walking_start = now + timedelta(days=1)
 walking_start = walking_start.replace(hour=10)
 walking_end = walking_start.replace(hour=10, minute=45)
 
-# filter events
-# if not event: create meal
 regular_event = RegularEvent.get_object(name=walking)
 Event.create(regular_event=regular_event.id, start=walking_start, end=walking_end, title={'value': walking})
 
-
+Message.simple_message(transport=NotificationTransport.desktop(), extra_data={'title': f'{walking} запланировано на завтра с 10:00 до 10:45'})
--- prod_planning_next_day.py
+++ prod_planning_next_day.py
@@ -2,6 +2,7 @@ import sys
 from datetime import timedelta
 
 from app.calendar.models import RegularEvent, Event
+from app.notification.models import Message, NotificationTransport
 
 sys.path.append('./')
 
@@ -21,4 +22,4 @@ walking_end = walking_start.replace(hour=10, minute=45)
 regular_event = RegularEvent.get_object(name=walking)
 Event.create(regular_event=regular_event.id, start=walking_start, end=walking_end, title={'value': walking})
 
-
+Message.simple_message(transport=NotificationTransport.desktop(), extra_data={'title': f'{walking} запланировано на завтра с 10:00 до 10:45'})

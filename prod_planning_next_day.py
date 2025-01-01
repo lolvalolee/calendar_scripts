8dbfdbd07025720c43e099c7872febc58bf321eb
oleksandr.piskurov <oleksandr.piskurov@mac-MT41YG9TJ6.local>
2025-01-01 07:10:30 +0200

    UPdate

---
+++ prod_planning_next_day.py
@@ -0,0 +1,24 @@
+import sys
+from datetime import timedelta
+
+from app.calendar.models import RegularEvent, Event
+
+sys.path.append('./')
+
+from app.profile.models import Profile
+
+
+profile = Profile.get()
+tz = profile.user_timezone
+now = profile.now
+now = now.replace(hour=7, minute=0, second=0, microsecond=0)
+
+walking = 'Ходьба не беговой'
+walking_start = now + timedelta(days=1)
+walking_start = walking_start.replace(hour=10)
+walking_end = walking_start.replace(hour=10, minute=45)
+
+regular_event = RegularEvent.get_object(name=walking)
+Event.create(regular_event=regular_event.id, start=walking_start, end=walking_end, title={'value': walking})
+
+

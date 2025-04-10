a78c845bb552822d40e8493086ca3f55597b1e5b
alexander <lolvalolee@gmail.com>
2025-04-10 16:31:21 +0300

    Added test changes

--- prod_day_result.py
+++ prod_day_result.py
@@ -1,10 +1,13 @@
 from datetime import datetime
 
 from app.habit.models import UserHabit
+from app.calendar.models import Event
 
 now = datetime.now()
 # '❌'
 
+event = Event.get_objects('/api/event/current/')
+print(event)
 habit, _ = UserHabit.get_objects()
 for item in habit:
     results, _ = item.results()

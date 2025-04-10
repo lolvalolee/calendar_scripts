4fb754e9758c0e59b604b207a613a2e821d7fcef
alexander <lolvalolee@gmail.com>
2025-04-10 17:07:00 +0300

    Added test changes

--- prod_day_result.py
+++ prod_day_result.py
@@ -1,11 +1,16 @@
 from datetime import datetime
+from zoneinfo import ZoneInfo
 
 from app.habit.models import UserHabit
 from app.calendar.models import Event
+from app.profile.models import Profile
 
 now = datetime.now()
 # '❌'
 
+profile = Profile.get()
+tz = ZoneInfo(profile.timezone)
+now = datetime.now(tz)
 events, _ = Event.get_objects('/api/event/current/')
 event = events[0]
 _now = datetime.now()

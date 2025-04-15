ff7090c9fc90c42a466c446764cd26d82abc3916
alexander <lolvalolee@gmail.com>
2025-04-15 06:32:35 +0300

    Added test changes

--- prod_day_result.py
+++ prod_day_result.py
@@ -24,7 +24,7 @@ result = 0
 profile = Profile.get()
 tz = ZoneInfo(profile.timezone)
 
-now = datetime.now(ZoneInfo('UTC')).astimezone(tz)
+now = datetime.now(datetime.timezone.utc)
 print(now)
 events, _ = Event.today_events()
 

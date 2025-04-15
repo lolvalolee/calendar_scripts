b7c33299e34d2b679666540670e49fb63dbe222f
alexander <lolvalolee@gmail.com>
2025-04-15 17:34:51 +0300

    Added test changes

--- prod_day_result.py
+++ prod_day_result.py
@@ -39,11 +39,11 @@ intervals = interval()
 
 for event in events:
     print('adding interval')
-    print(interval[max(tomorrow, event.start).timestamp(), min(
+    print(interval[max(today, event.start).timestamp(), min(
         tomorrow, event.end or datetime.max.astimezone(tz)).timestamp()])
     print(event.start, event.end)
     print('********************')
-    intervals = intervals | interval[max(tomorrow, event.start).timestamp(), min(
+    intervals = intervals | interval[max(today, event.start).timestamp(), min(
         tomorrow, event.end or datetime.max.astimezone(tz)).timestamp()]
 
 total = sum(map(lambda _item: _item[1] - _item[0], intervals)) // 10

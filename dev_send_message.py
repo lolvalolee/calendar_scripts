b453321a72797bb01f718ed2d9ec352c9db83bd1
alexander <lolvalolee@gmail.com>
2025-06-23 21:42:58 +0300

    Update

--- dev_send_message.py
+++ dev_send_message.py
@@ -2,8 +2,8 @@ from app.notification.models import NotificationTransport
 from app.social.models import Friend
 
 
-transport = NotificationTransport.desktop()
+transport = NotificationTransport.telegram()
 friends, _ = Friend.get_objects()
 
 for item in friends:
-    print(item.send_simple_message('here is text:)', transport))
+    print(item.send_simple_message('кусь!', transport))

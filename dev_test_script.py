9b0f3a2577f1369388ecc87513350bad014b77a2
alexander <lolvalolee@gmail.com>
2025-07-06 22:08:58 +0300

    Update

--- dev_test_script.py
+++ dev_test_script.py
@@ -10,27 +10,27 @@ for item in UserStatus.get_objects():
     print(item.label)
 
 
-extra_data = {
-    'title': 'Начать событие сон?',
-    'questions': [
-        {
-            'title': 'Нет',
-            'action': {
-                'type': 'delete_notification',
-                'qs': {'name': 'Сон'}
-            }
-        },
-        {
-            'title': 'Сьел!',
-            'action': {
-                'type': 'call_handler',
-                'qs': {'id': 9},
-                'handler_extra_data': {
-                    'test_key': 'test_value'
-                }
-            }
-        },
-    ]
-}
+# extra_data = {
+#     'title': 'Начать событие сон?',
+#     'questions': [
+#         {
+#             'title': 'Нет',
+#             'action': {
+#                 'type': 'delete_notification',
+#                 'qs': {'name': 'Сон'}
+#             }
+#         },
+#         {
+#             'title': 'Сьел!',
+#             'action': {
+#                 'type': 'call_handler',
+#                 'qs': {'id': 9},
+#                 'handler_extra_data': {
+#                     'test_key': 'test_value'
+#                 }
+#             }
+#         },
+#     ]
+# }
 
-Message.question(transport=NotificationTransport.desktop(), extra_data=extra_data)
+# Message.question(transport=NotificationTransport.desktop(), extra_data=extra_data)

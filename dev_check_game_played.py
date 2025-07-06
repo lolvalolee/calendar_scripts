170971d326d8a7ea879868ec0fa10722797ace6b
alexander <lolvalolee@gmail.com>
2025-07-06 14:38:19 +0300

    Update

--- dev_check_game_played.py
+++ dev_check_game_played.py
@@ -1,4 +1,7 @@
 import sys
+
+import requests
+
 from datetime import timedelta
 
 from app.calendar.models import RegularEvent, Event
@@ -12,3 +15,26 @@ profile = Profile.get()
 
 steam_api_key = ApiKey.get_object(name='steam')
 print(steam_api_key)
+
+steam_id = "76561198082140903"  # Замените на ваш SteamID
+api_key = steam_api_key.key
+
+url = f"http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={api_key}&steamids={steam_id}"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+    if "response" in data and "players" in data["response"]:
+        player = data["response"]["players"][0]
+
+        if "gameextrainfo" in player:
+            game = player["gameextrainfo"]
+            print(f"Вы играете в игру: {game}")
+        else:
+            print("Вы сейчас не играете в игру.")
+    else:
+        print("Информация о пользователе не найдена.")
+else:
+    print("Ошибка при выполнении запроса.")

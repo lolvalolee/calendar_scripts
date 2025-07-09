8f41e11368ca5236716738c4777a9457adffdc98
alexander <lolvalolee@gmail.com>
2025-07-09 18:26:21 +0300

    Update

--- dev_set_current_mood.py
+++ dev_set_current_mood.py
@@ -2,15 +2,11 @@ import json
 import os
 import sys
 
-from app.notification.models import Message, NotificationTransport
 from app.profile.models import UserStatus
+
 
 sys.path.append('./')
 
 statuses, _ = UserStatus.get_objects()
 
-print(os.environ.get('handler_extra_data') == 'хорошее настроение')
 data = json.loads(os.environ.get('handler_extra_data'))
-print(data)
-print(data['mood'])
-print(data['mood'] == 'хорошее настроение')

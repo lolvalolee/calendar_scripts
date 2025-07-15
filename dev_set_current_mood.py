c0c761298f560aec1813d0068bc77228482a1afb
alexander <lolvalolee@gmail.com>
2025-07-15 21:40:07 +0300

    Update

--- dev_set_current_mood.py
+++ dev_set_current_mood.py
@@ -1,13 +1,53 @@
 import json
 import os
+import re
 import sys
+sys.path.append('./')
 
 from app.profile.models import UserStatus
 
+import json
+import os
+import re
+import sys
 
-sys.path.append('./')
+HABIT_ACTION_REPORT = 'отметь'
 
-statuses, _ = UserStatus.get_objects()
+regexps = [
+    f'^(?P<action>{HABIT_ACTION_REPORT}) привычку (?P<habit_name>.+) (?P<result>выполнена|отменена|провалена)',
+    r'^((?P<action>начни|закончи) (?P<regular>регулярное )?событие (?P<event_name>.+))'
+]
 
-data = json.loads(os.environ.get('handler_extra_data'))
-print(data)
+# text = "отметь привычку Тренировка выполнена"
+text = json.loads(os.environ.get('handler_extra_data'))
+
+
+class CommandHandler:
+    cmd = None
+    match = None
+
+    def __init__(self, cmd):
+        self.cmd = cmd
+
+        for regex in regexps:
+            match = re.match(regex, text)
+            if match:
+                self.match = match
+                break
+
+    def handle_habit_report(self, *args, **options):
+        print('hey!')
+
+    def handle(self, *args, **options):
+        groupdict = self.match.groupdict()
+        action = groupdict['action']
+
+        handlers_map = {
+            HABIT_ACTION_REPORT: self.handle_habit_report,
+        }
+        handlers_map[action](*args, **options)
+
+
+cmd = CommandHandler(text)
+cmd.handle()
+

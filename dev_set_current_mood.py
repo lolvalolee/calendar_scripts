e30b9580ea282912c7ddd1f4fa4d85de617db97f
alexander <lolvalolee@gmail.com>
2025-07-21 01:28:48 +0300

    Update

--- dev_set_current_mood.py
+++ dev_set_current_mood.py
@@ -3,46 +3,12 @@ import os
 import re
 import sys
 
+from app.profile.models import UserStatus
 from handlers.constants import HABIT_ACTION_REPORT, VOICE_COMMAND_REGEXPS, ACTION_EVENT_START
 from handlers.event_voice_command import start_event
 from handlers.habit_voice_command import handle_habit_report
 
 sys.path.append('./')
 
-from app.habit.constants import RESULT_COMPLETED, RESULT_FAILED
-from app.notification.models import Message, NotificationTransport
-
-# text = "отметь привычку Тренировка выполнена"
-text = json.loads(os.environ.get('handler_extra_data'))['voice_command'].lower()
-
-
-class CommandHandler:
-    cmd = None
-    match = None
-
-    def __init__(self, cmd):
-        self.cmd = cmd
-
-        for regex in VOICE_COMMAND_REGEXPS:
-            match = re.match(regex, text)
-            if match:
-                self.match = match
-                break
-        if not match:
-            Message.simple_message(transport=NotificationTransport.telegram(),
-                                   extra_data={'title': f'Команду невозможно обработать: {text}'})
-            exit(0)
-
-    def handle(self, *args, **options):
-        groupdict = self.match.groupdict()
-        action = groupdict['action']
-
-        handlers_map = {
-            HABIT_ACTION_REPORT: handle_habit_report,
-            ACTION_EVENT_START: start_event,
-        }
-        handlers_map[action](self.match)
-
-
-cmd = CommandHandler(text)
-cmd.handle()
+statuses, _ = UserStatus.get_objects()
+print(statuses)
--- handlers/event_voice_command.py
+++ handlers/event_voice_command.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from app.calendar.models import RegularEvent, Event
 from app.notification.models import NotificationTransport, Message
 
+
 _event_name_mapping = {
     'задротил': ['затратил', ]
 }
@@ -10,7 +11,6 @@ _event_name_mapping = {
 event_name_mapping = {}
 [event_name_mapping.update({_value: key for _value in value}) for key, value in _event_name_mapping.items()]
 event_name_mapping.update({key: key for key in _event_name_mapping.keys()})
-
 
 
 def start_event(match):

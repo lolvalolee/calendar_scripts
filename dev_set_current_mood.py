6397e270cd35e5e54cc278de47d83091eea62082
alexander <lolvalolee@gmail.com>
2025-07-21 01:40:49 +0300

    Update

--- dev_set_current_mood.py
+++ dev_set_current_mood.py
@@ -3,6 +3,8 @@ import os
 import re
 import sys
 
+from app.notification.constants import BUTTON_VARIANT_WARNING
+from app.notification.models import Message, NotificationTransport
 from app.profile.models import UserStatus
 from handlers.constants import HABIT_ACTION_REPORT, VOICE_COMMAND_REGEXPS, ACTION_EVENT_START
 from handlers.event_voice_command import start_event
@@ -11,4 +13,21 @@ from handlers.habit_voice_command import handle_habit_report
 sys.path.append('./')
 
 statuses, _ = UserStatus.get_objects()
-print(statuses)
+
+questions = [
+    {
+        'title': 'Нет',
+        'style': BUTTON_VARIANT_WARNING,
+        'action': {
+            'type': 'call_handler',
+            'qs': {'name': 'Сон'}
+        }
+    } for item in statuses
+]
+
+extra_data = {
+    'title': 'Начать событие сон?',
+    'questions': questions
+}
+
+Message.question(transport=NotificationTransport.desktop(), extra_data=extra_data)

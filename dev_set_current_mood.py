6e91fe9eb2331fe2ddb9ad1c6bceaf790689f2dc
alexander <lolvalolee@gmail.com>
2025-08-03 00:49:42 +0300

    Update

--- dev_set_current_mood.py
+++ dev_set_current_mood.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 from app.calendar.models import Event, RegularEvent
 from app.notification.constants import BUTTON_VARIANT_WARNING
 from app.notification.models import Message, NotificationTransport
+from app.stockRoom.models import Recipe
 from utils.misc import get_handler_extra_data
 
 
@@ -16,18 +17,20 @@ _now = datetime.now()
 if mood == 'Настроение: хорошее':
     RegularEvent.get_object(name='утренний кофе').start(start_dt=_now + timedelta(minutes=10))
 
+    recipes, _ = Recipe.get_objects(tag='завтрак')
+
     questions = [
         {
-            'title': item,
+            'title': recipe.stock_room_item.name['value'],
             'style': BUTTON_VARIANT_WARNING,
             'action': {
                 'type': 'call_handler',
                 'qs': {'name': 'dev_plane_breakfast.py'},
                 'handler_extra_data': {
-                    'meal': item
+                    'meal_id': recipe.id
                 }
             }
-        } for item in ['каша гречневая', 'омлет', 'просто чай с шоколадкой']
+        } for recipe in recipes
     ]
 
     extra_data = {

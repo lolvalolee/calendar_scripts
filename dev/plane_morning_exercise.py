85a90db55e9c1d59e63c25f55a37f95aa92dd011
alexander <lolvalolee@gmail.com>
2025-08-04 15:36:48 +0300

    Update

--- dev/plane_morning_exercise.py
+++ dev/plane_morning_exercise.py
@@ -1,5 +1,10 @@
 import sys
 
+from app.notification.constants import BUTTON_VARIANT_WARNING
+from app.notification.models import Message, NotificationTransport
+from app.profile.models import UserStatus
+
+
 sys.path.append('../')
 
 print('ee')

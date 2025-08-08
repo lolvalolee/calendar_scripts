38a2034d005ab936d6a80e355f467952a6772ef8
alexander <lolvalolee@gmail.com>
2025-08-08 18:16:24 +0300

    Update

--- dev/stock_room_item_added.py
+++ dev/stock_room_item_added.py
@@ -1,9 +1,10 @@
 import os
 
 from utils.misc import get_handler_extra_data
-from app.stockRoom.models import UserStockRoomItem
+from app.stockRoom.models import UserStockRoomItem, StockItem
 
 
 def handle():
     item = UserStockRoomItem.get_object(os.environ['object_id'])
-    print(item)
+    stock_item = StockItem.get_object(item.stock_room_item['id'])
+    print(item, stock_item)

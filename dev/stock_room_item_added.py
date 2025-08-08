f7bd41527071d65e7c2bd39dde995d6f2cbc6cc2
alexander <lolvalolee@gmail.com>
2025-08-08 18:46:06 +0300

    Update

--- dev/stock_room_item_added.py
+++ dev/stock_room_item_added.py
@@ -8,5 +8,5 @@ def handle():
     item = UserStockRoomItem.get_object(os.environ['object_id'])
     stock_item = StockItem.get_object(item.stock_room_item['id'])
     for tag in stock_item.tags:
-        if tag.name == 'есть срок годности':
+        if tag['name'] == 'есть срок годности':
             print('hey')

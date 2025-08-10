6eff9b161ba4fe5b5074f086083030c558de164b
alexander <lolvalolee@gmail.com>
2025-08-10 14:29:56 +0300

    Update

--- dev/notify_food_exp_date.py
+++ dev/notify_food_exp_date.py
@@ -10,6 +10,6 @@ def handle():
     start = profile.now
     end = start + timedelta(hours=24)
     items, cnt = UserStockRoomItem.get_objects(exp_date__lte=end, exp_date__gte=start, status=STATUS_IN_STOCK_ROOM)
-    items = ' '.join([item.stock_room_item.name['value'] for item in items])
+    items = ' '.join([item.stock_room_item['name']['value'] for item in items])
     msg = f'У {items} сегодня заканчивается срок годности. Не забудь!'
     print(items)

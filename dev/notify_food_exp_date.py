2f220fa7b05228ca10859d43770e82eb05c54eae
alexander <lolvalolee@gmail.com>
2025-08-10 13:52:05 +0300

    Update

---
+++ dev/notify_food_exp_date.py
@@ -0,0 +1,13 @@
+from datetime import timedelta
+
+from app.profile.models import Profile
+from app.stockRoom.constants import STATUS_IN_STOCK_ROOM
+from app.stockRoom.models import UserStockRoomItem
+
+
+def handle():
+    profile = Profile.get()
+    start = profile.now
+    end = start + timedelta(hours=24)
+    items, cnt = UserStockRoomItem.get_objects(exp_date__lte=end, exp_date__gte=start, status=STATUS_IN_STOCK_ROOM)
+    print(items)

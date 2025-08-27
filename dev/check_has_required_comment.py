73c29f747636f810d8ade3f4e6e458b43dbad6ed
alexander <lolvalolee@gmail.com>
2025-08-27 18:27:55 +0300

    Update

--- dev/check_has_required_comment.py
+++ dev/check_has_required_comment.py
@@ -6,6 +6,6 @@ def handle():
     objects, _ = UserHabitRecord.get_objects(extra_data__note_required=True)
     rs = Comment.create(text={'value': 'text here'}, tags=['tag1', 'tag2'])
     for obj in objects:
-        print(obj)
+        print(obj.extra_data)
         # extra_data = {'note_required': True, 'uuid': str(uuid.uuid4())}
         # rs = Comment.create(text={'value': 'text here'}, tags=['tag1', 'tag2'])

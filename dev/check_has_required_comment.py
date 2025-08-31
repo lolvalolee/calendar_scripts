a988de46dfbb31f0bd74c9e471087afacadbdb8b
alexander <lolvalolee@gmail.com>
2025-08-31 20:33:45 +0300

    Update

--- dev/check_has_required_comment.py
+++ dev/check_has_required_comment.py
@@ -8,8 +8,8 @@ def handle():
     for obj in objects:
         if not Comment.exists(extra_data__uuid=obj.extra_data['uuid']):
             for transport in [NotificationTransport.telegram, NotificationTransport.desktop]:
-                Message.simple_message(
-                    f'Ты забил на одно из запланировных дел. Обьясни в чем причина. Создай нотификацию с uuid {obj.extra_data["uuid"]}', transport=transport)
+                Message.simple_messagev2(
+                    f'Ты забил на одно из запланировных дел. Обьясни в чем причина. Создай нотификацию с uuid {obj.extra_data["uuid"]}', transport)
             print('comment not exist', obj.extra_data)
         else:
             print('comment exist', obj.extra_data['uuid'])

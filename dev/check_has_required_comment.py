b41027a1cd6ebe10f90ddce45f7b6ead787b5c3d
alexander <lolvalolee@gmail.com>
2025-08-31 19:14:54 +0300

    Update

--- dev/check_has_required_comment.py
+++ dev/check_has_required_comment.py
@@ -6,5 +6,5 @@ def handle():
     objects, _ = UserHabitRecord.get_objects(extra_data__note_required=True)
     for obj in objects:
         print(obj.extra_data)
-        if not  Comment.exists(extra_data__uuid=obj.extra_data['uuid']):
+        if not Comment.exists(extra_data__uuid=obj.extra_data['uuid']):
             print('comment not exist', obj.extra_data)

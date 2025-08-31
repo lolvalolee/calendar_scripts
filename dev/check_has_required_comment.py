eb48618a04b660b5829f2fae5fd433f8f98d2216
alexander <lolvalolee@gmail.com>
2025-08-31 20:30:40 +0300

    Update

--- dev/check_has_required_comment.py
+++ dev/check_has_required_comment.py
@@ -1,11 +1,15 @@
 from app.habit.models import UserHabitRecord
 from app.tag.models import Comment
+from app.notification.models import Message, NotificationTransport
 
 
 def handle():
     objects, _ = UserHabitRecord.get_objects(extra_data__note_required=True)
     for obj in objects:
         if not Comment.exists(extra_data__uuid=obj.extra_data['uuid']):
+            for transport in [NotificationTransport.telegram, NotificationTransport.desktop]:
+                Message.simple_message(
+                    f'Ты забил на одно из запланировных дел. Обьясни в чем причина. Создай нотификацию с uuid {obj.extra_data["uuid"]}', transport=transport)
             print('comment not exist', obj.extra_data)
         else:
             print('comment exist', obj.extra_data['uuid'])

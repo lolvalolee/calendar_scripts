0a863e039e222755e621f71d03bd387779410dd6
alexander <lolvalolee@gmail.com>
2025-11-25 13:12:14 +0200

    Update

--- dev/dev_take_sweats.py
+++ dev/dev_take_sweats.py
@@ -2,10 +2,17 @@ from datetime import timedelta
 
 from app.calendar.models import RegularEvent
 from app.profile.models import Profile
+from app.reward.models import PointType
 
 
 def handle():
     profile = Profile.get()
+
+    point_type = PointType.get_object(name='Сладости')
+    if point_type.total_amount > 0:
+        print('too low!')
+        exit(0)
+
     start = profile.now
     end = start + timedelta(minutes=15)
     regular_event = RegularEvent.get_object(name='Есть вкусняшки')

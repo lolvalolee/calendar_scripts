25f4cd72dc095e32ce69a778ba8e7574ab81d4c0
alexander <lolvalolee@gmail.com>
2025-11-25 12:43:31 +0200

    Update

--- dev/dev_take_sweats.py
+++ dev/dev_take_sweats.py
@@ -1,6 +1,12 @@
+from datetime import timedelta
+
 from app.calendar.models import RegularEvent
+from app.profile.models import Profile
+
 
 def handle():
+    profile = Profile.get()
+    start = profile.now
+    end = start + timedelta(minutes=15)
     regular_event = RegularEvent.get_object(name='Есть вкусняшки')
-    regular_event.start()
-    pass
+    regular_event.start(start_dt=start, end_dt=end)

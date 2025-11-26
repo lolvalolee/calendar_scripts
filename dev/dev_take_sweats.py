07a5c07a84faa000752d75b1ab4dc8a50932d5a7
alexander <lolvalolee@gmail.com>
2025-11-26 14:51:59 +0200

    Update

--- dev/dev_take_sweats.py
+++ dev/dev_take_sweats.py
@@ -3,6 +3,7 @@ from datetime import timedelta
 from app.calendar.models import RegularEvent
 from app.profile.models import Profile
 from app.reward.models import PointType, Reward
+from app.notification.models import Message, NotificationTransport
 from app.tag.models import Comment
 
 
@@ -10,10 +11,9 @@ def handle():
     profile = Profile.get()
 
     notes, _ = Comment.get_objects(tag=['тренировка','совет'])
-    print(notes)
     text = 'Через 10 минут треша. Не забывай: \n' + '\n'.join([f'- {note.text["value"]}' for note in notes])
 
-    print(text)
+    Message.simple_messagev2(transport=NotificationTransport.telegram(), title=text)
     exit(0)
 
     point_type = PointType.get_object(name='Сладости')

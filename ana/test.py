831b63f8476f10301fd5dbea640d9432498bdc65
alexander <lolvalolee@gmail.com>
2026-01-25 11:39:52 +0200

    Update

--- ana/test.py
+++ ana/test.py
@@ -1,7 +1,32 @@
+from app.handler.constants import ACTION_CALL_HANDLER
 from app.notification.models import Message, NotificationTransport
-from app.profile.models import Profile
 
 
 def handle():
-    profile = Profile.get()
-    Message.simple_messagev2('test message', NotificationTransport.telegram())
+    # Message.simple_messagev2('test message', NotificationTransport.telegram())
+
+    questions = [
+        {
+            'title': 'Кусь',
+            'action': {
+                'type': ACTION_CALL_HANDLER,
+                'qs': {'id': 12},
+                'handler_extra_data': {'i': '-'}
+            }
+        },
+        {
+            'title': 'Грызь!',
+            'action': {
+                'type': ACTION_CALL_HANDLER,
+                'qs': {'id': 12},
+                'handler_extra_data': {'i': '+'}
+            }
+        },
+    ]
+
+    extra_data = {
+        'title': 'Кусь или грызь?',
+        'questions': questions
+    }
+
+    Message.question(transport=NotificationTransport.desktop(), extra_data=extra_data)

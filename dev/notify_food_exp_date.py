bbc6a3b83a8deeff672f64ff94d139693c2d3e2c
alexander <lolvalolee@gmail.com>
2026-01-25 11:10:18 +0200

    Update

--- dev/notify_food_exp_date.py
+++ dev/notify_food_exp_date.py
@@ -10,23 +10,4 @@ from app.stockRoom.models import UserStockRoomItem
 
 def handle():
     profile = Profile.get()
-    start = profile.now
-    print(Event.current_events())
-    exit(0)
-    # items, cnt = UserStockRoomItem.get_objects(exp_date__lte=end, exp_date__gte=start, status=STATUS_IN_STOCK_ROOM)
-    # if not cnt:
-    #     exit(0)
-
-    items, cnt = UserStockRoomItem.get_objects()
-    # if not cnt:
-    #     exit(0)
-
-    items = ' '.join([item.stock_room_item['name']['value'] for item in items])
-    msg = f'У {items} сегодня заканчивается срок годности. Не забудь!'
-    Message.simple_messagev2(transport=NotificationTransport.telegram(), title=msg)
-
-    regular_event = RegularEvent.get_object(name='готовка')
-    subtask, created = regular_event.create_subtask(
-        title={'value': f'Приготовить {msg}. Ну или выкинуть'}, extra_data={'test': datetime.now().isoformat()})
-    if created:
-        MainPageDisplay.assign(subtask)
+    Message.simple_messagev2(transport=NotificationTransport.telegram(), title='test message')

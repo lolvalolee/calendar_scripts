5356724b4538d26a86296bf4295ca7e4a17640fd
alexander <lolvalolee@gmail.com>
2026-02-08 22:17:02 +0200

    Update

---
+++ dev/test.py
@@ -0,0 +1,32 @@
+from app.handler.constants import ACTION_CALL_HANDLER
+from app.notification.models import Message, NotificationTransport
+
+
+def handle():
+    # Message.simple_messagev2('test message', NotificationTransport.telegram())
+
+    questions = [
+        {
+            'title': 'Кусь',
+            'action': {
+                'type': ACTION_CALL_HANDLER,
+                'qs': {'name': 'test'},
+                'handler_extra_data': {'i': '-'}
+            }
+        },
+        {
+            'title': 'Грызь!',
+            'action': {
+                'type': ACTION_CALL_HANDLER,
+                'qs': {'name': 'test'},
+                'handler_extra_data': {'i': '+'}
+            }
+        },
+    ]
+
+    extra_data = {
+        'title': 'Кусь или грызь?',
+        'questions': questions
+    }
+
+    Message.question(transport=NotificationTransport.telegram(), extra_data=extra_data)

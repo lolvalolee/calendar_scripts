525b42aec2afb65c5d77f2462361c0efdc5d2872
alexander <lolvalolee@gmail.com>
2026-02-09 21:46:10 +0200

    Update

--- dev/test.py
+++ dev/test.py
@@ -1,34 +1,25 @@
-import os
-
 from app.handler.constants import ACTION_CALL_HANDLER
 from app.notification.models import Message, NotificationTransport
 from utils.misc import get_handler_extra_data
 
 
 def handle():
-    # Message.simple_messagev2('test message', NotificationTransport.telegram())
     print(get_handler_extra_data())
+    meal = ['opti meal', 'purina']
     questions = [
         {
             'title': 'Кусь',
             'action': {
                 'type': ACTION_CALL_HANDLER,
                 'qs': {'name': 'test'},
-                'handler_extra_data': {'i': '-'}
+                'handler_extra_data': {'i': item}
             }
-        },
-        {
-            'title': 'Грызь!',
-            'action': {
-                'type': ACTION_CALL_HANDLER,
-                'qs': {'name': 'test'},
-                'handler_extra_data': {'i': '+'}
-            }
-        },
+        }
+        for item in meal
     ]
 
     extra_data = {
-        'title': 'Кусь или грызь?',
+        'title': 'Чем сегодня задабривал зверя?',
         'questions': questions
     }
 

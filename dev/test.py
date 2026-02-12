5feb94a5893ce96b9b65883440a82cfcc5e1348c
alexander <lolvalolee@gmail.com>
2026-02-12 20:07:07 +0200

    Update

--- dev/test.py
+++ dev/test.py
@@ -14,7 +14,7 @@ def handle():
             exit(0)
 
             stock = Stock.get_object(id=1)
-            measure = Measure.get_object(name='штук')
+            measure = Measure.get_object(name=data['m'])
             stock_room_item = StockItem.get_object(name=data['i'], stock=stock.id)
             print(stock.use(stock_room_item_id=stock_room_item.id, measure_id=measure.id, count=1))
         else:
@@ -39,7 +39,32 @@ def handle():
 
         exit(0)
 
-    meal = ['opti meal', 'purina', 'авфавыаы']
+    stock = Stock.get_object(id=1)
+
+    questions = [
+        {
+            'title': item.name['value'],
+            'action': {
+                'type': ACTION_CALL_HANDLER,
+                'qs': {'name': 'test'},
+                'handler_extra_data': {'i': item.id}
+            }
+        }
+        for item in StockItem.get_objects(stock=stock.id)
+    ]
+
+    extra_data = {
+        'title': 'Предметы склада',
+        'questions': questions
+    }
+
+    Message.question(transport=NotificationTransport.telegram(), extra_data=extra_data)
+
+    exit(0)
+
+
+
+    meal =  ['opti meal', 'purina', 'авфавыаы']
     questions = [
         {
             'title': item,

08d95c571111a871875166962bdf1bd1cd13fdde
alexander <lolvalolee@gmail.com>
2026-02-23 14:18:29 +0200

    Update

---
+++ prod/check_had_breakfast.py
@@ -0,0 +1,10 @@
+from app.profile.models import Profile
+from app.calendar.models import RegularEvent
+
+
+def handle():
+    profile = Profile.get()
+    now = profile.now
+
+    regular_event = RegularEvent.get_object(name='завтрак')
+    print(regular_event)

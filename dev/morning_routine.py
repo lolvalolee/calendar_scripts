6cd43a6befebd65a305651d1f0c2a7bd0f9f3533
alexander <lolvalolee@gmail.com>
2026-05-23 20:11:56 +0300

    Update

--- dev/morning_routine.py
+++
@@ -1,9 +0,0 @@
-from app.profile.models import Profile
-from app.notification.models import Message, NotificationTransport
-
-
-def handle():
-    profile = Profile.get()
-
-    Message.simple_messagev2('Почисти зубы!', NotificationTransport.telegram())
-    exit(0)

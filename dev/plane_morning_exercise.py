6bf628d00fc887e2a5aa29c9b658903b9a66aeed
alexander <lolvalolee@gmail.com>
2026-06-02 15:57:58 +0300

    Update

--- dev/plane_morning_exercise.py
+++ dev/plane_morning_exercise.py
@@ -5,5 +5,5 @@ from utils.misc import get_handler_extra_data
 
 def handle():
     exercises = UserExercise.get_objects()
-    print(exercises)
+    print( list(exercises))
     print(get_handler_extra_data())

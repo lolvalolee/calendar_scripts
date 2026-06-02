1a83f350d7c5a96459f9fed83c6031b4d56df9da
alexander <lolvalolee@gmail.com>
2026-06-02 18:34:44 +0300

    Update

--- dev/plane_morning_exercise.py
+++ dev/plane_morning_exercise.py
@@ -4,7 +4,7 @@ from utils.misc import get_handler_extra_data
 
 
 def handle():
-    exercises = list(UserExercise.get_objects())
+    exercises = list(UserExercise.get_objects(extra_data__count=5))
     print(UserTraining.create(user_training_exercises=[
         {'user_exercise': item.id, 'count': 1} for item in exercises
     ]))
